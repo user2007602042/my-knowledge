73bb20e4c4ee1df5f568c31c81761ff44ba21b3b
Seungmin Han <b23070.tdc.hanmacgroup@gmail.com>
2026-03-17 10:29:20 +0900

    Quartz sync: Mar 17, 2026, 10:29 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "지식저장소",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
